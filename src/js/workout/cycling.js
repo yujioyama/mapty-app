d670eaffc6c60007712215a0eab20bd72464c1cb
yujioyama <yujioyamanyc@gmail.com>
2023-06-20 23:14:34 +0900

    separated files

---
+++ src/js/workout/cycling.js
@@ -0,0 +1,17 @@
+import Workout from "./workout";
+
+export default class Cycling extends Workout {
+  type = "cycling";
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+    this._setDescription();
+  }
+
+  calcSpeed() {
+    // km/h
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
---
+++ src/js/workout/running.js
@@ -0,0 +1,17 @@
+import Workout from "./workout";
+
+export default class Running extends Workout {
+  type = "running";
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+    this._setDescription();
+  }
+
+  calcPace() {
+    // min/km
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
